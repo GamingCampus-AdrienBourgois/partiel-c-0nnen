d20a3bf9c0b3ff8bcd274fcec95a2ac360b1f9e3
0nnen <onnenennon@gmail.com>
2024-01-19 13:28:26 +0100

    Update Solution1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,48 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
-}
+    if (_from == _to) {
+
+        return _value;
+    }
+
+    switch (_from) {
+
+    // CELSIUS to ...
+    case TemperatureUnits::CELSIUS:
+        switch (_to)
+        {
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value * 9.0f / 5.0f) + 32.0f;
+        case TemperatureUnits::KELVIN:
+            return _value + 273.0f;
+        }
+        break;
+
+    // FAHRENHEIT to ...
+    case TemperatureUnits::FAHRENHEIT:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return (_value - 32.0f) * 5.0f / 9.0f;
+        case TemperatureUnits::KELVIN:
+            return (_value - 32.0f) * 5.0f / 9.0f + 273.0f;
+        }
+        break;
+
+    // KELIVIN to ...
+    case TemperatureUnits::KELVIN:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return _value - 273.0f;
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value - 273.0f) * 9.0f / 5.0f + 32.0f;
+        }
+        break;
+    }
+
+    return -1.0f;
+};
 
 #endif
